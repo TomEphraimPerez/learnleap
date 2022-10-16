6e45c4415a65951671ab5891660991e00aded0be
tperez35 <tperez35@gatech.edu>
2022-10-15 19:27:48 -0700

    More 'realistic' attr values

--- plasticityCD4toTh-2.py
+++ plasticityCD4toTh-2.py
@@ -3,7 +3,7 @@
                             # attributes to the BIG-8: AED + plasticity, current, expression.
                             
 # A CQM solver prob on a simple mixed-integer linear-programming, MILP, type of optimization problem.
- # https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-diet-reals ††
+ # https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-Thelp-reals ††
 # imports
 import dimod as dimod       # thx red lightbulb
 from dwave.system import LeapHybridCQMSampler         #o
@@ -11,45 +11,43 @@ from dwave.system import LeapHybridCQMSampler         #o
   # For charges - $$$
     # Select a solver
     # sampler = LeapHybridSampler()
-
     # Submit for solution
     # answer = sampler.sample_qubo(Q)
 
 # Python 3.9.2
 
-print('\nThis is the diet optimization prob w Expression v Plasticity goal, gvn several ')
+print('\nThis is the Thelp optimization prob w Expression v Plasticity goal, gvn several ')
 print('constraints.')
 
 # Pts dict of Pts' Pts.
-
 # attoamps -> picoamp = 10^-12a, femtoamps =10^-15a, attoamps = 10^-18a.
-Pts = {'Tfh': {'attoamps': 3, 'ACTivation': 1, 'EXPansion': 22,
-                    'DIFFerentiation': 2, 'Expression': 7, 'Plasticity': 2.5, 'Units': 'continuous'},
-        'Th9': {'attoamps': 17, 'ACTivation': 5, 'EXPansion': 3,
-                    'DIFFerentiation': 2, 'Expression': 2, 'Plasticity': 4.0, 'Units': 'continuous'},
-        'Th2': {'attoamps': 1, 'ACTivation': 2, 'EXPansion': 23, 'DIFFerentiation': 3,
-                    'Expression': 10, 'Plasticity': 1.0, 'Units': 'discrete'},
-        'iTreg': {'attoamps': 9, 'ACTivation': 4, 'EXPansion': 25, 'DIFFerentiation': 4,
+Pts = {'Tfh': {'attoamps': 0.39, 'ACTivation': 1, 'EXPansion': 22, 'DIFFerentiation': 2,
+                    'Expression': 7, 'Plasticity': 2.5, 'Units': 'continuous'},
+        'Th9': {'attoamps': 0.17, 'ACTivation': 5, 'EXPansion': 3, 'DIFFerentiation': 2,
+                    'Expression': 2, 'Plasticity': 4.0, 'Units': 'continuous'},
+        'Th2': {'attoamps': 0.11, 'ACTivation': 2, 'EXPansion': 23, 'DIFFerentiation': 3,
+                    'Expression': 10, 'Plasticity': 1.0, 'Units': 'continuous'},
+        'iTreg': {'attoamps': 0.91, 'ACTivation': 4, 'EXPansion': 25, 'DIFFerentiation': 4,
                     'Expression': 3, 'Plasticity': 1.3, 'Units': 'continuous'},
-        'Tr1': {'attoamps': 9, 'ACTivation': 3, 'EXPansion': 50, 'DIFFerentiation': 3,
+        'Tr1': {'attoamps': 0.83, 'ACTivation': 3, 'EXPansion': 50, 'DIFFerentiation': 3,
                     'Expression': 5, 'Plasticity': 0.25, 'Units': 'continuous'},
-        'Th22': {'attoamps': 4, 'ACTivation': 1, 'EXPansion': 20, 'DIFFerentiation': 14,
-                    'Expression': 5, 'Plasticity': 2.0, 'Units': 'discrete'},
-        'Th17': {'attoamps': 4, 'ACTivation': 4, 'EXPansion': 20, 'DIFFerentiation': 14,
-                    'Expression': 5, 'Plasticity': 2.0, 'Units': 'discrete'},
-        'Th1': {'attoamps': 5, 'ACTivation': 5, 'EXPansion': 21, 'DIFFerentiation': 15,
-                    'Expression': 6, 'Plasticity': 3.0, 'Units': 'discrete'}}
+        'Th22': {'attoamps': 0.44, 'ACTivation': 1, 'EXPansion': 20, 'DIFFerentiation': 14,
+                    'Expression': 5, 'Plasticity': 2.0, 'Units': 'continuous'},
+        'Th17': {'attoamps': 0.36, 'ACTivation': 4, 'EXPansion': 28, 'DIFFerentiation': 14,
+                    'Expression': 5, 'Plasticity': 2.0, 'Units': 'continuous'},
+        'Th1': {'attoamps': 0.52, 'ACTivation': 5, 'EXPansion': 12, 'DIFFerentiation': 15,
+                    'Expression': 6, 'Plasticity': 3.0, 'Units': 'continuous'}}
 
 
-min_nutrients = {"attoamps": 50, "ACTivation": 30, "EXPansion": 130, "DIFFerentiation": 30}
-max_calories = 2000                                     # for setting up bounds. See 12 lines below.
+min_attributes = {"attoamps": 0.1, "ACTivation": 1, "EXPansion": 1, "DIFFerentiation": 1}
+max_attoamps = 0.99                                     # for setting up bounds. See 12 lines below.
 
 # quantities list | dimod is a shared API for samplers and provides classes for eg., QM's
   # inc higher-order non-quadratic models.
 quantities = [dimod.Real(f"{Pt}") if Pts[Pt]["Units"] == "continuous" # an f-string. '{Pt}'
                                                         # will be replaced by a value.
     else dimod.Integer(f"{Pt}")
-              for Pt in Pts.keys()]                    # key = eg cals : value = 20
+              for Pt in Pts.keys()]                     # key = eg cals : value = 20
 
 print("\nSimply showing ex of a lin bias. ")
 print(quantities[0])                                    # simple linear bias
@@ -58,7 +56,7 @@ print(2*quantities[0])                                  # Now dbl lin bias
 #print(quantities[0] * quantities[1]) #Now a quadratic bias. # ValueError: REAL variables
                                                             # (e.g. 'Tfh') cannot have interactions
 for ind, Pt in enumerate(Pts.keys()):
-    ub = max_calories / Pts[Pt]["ACTivation"] # upper bnd is 20 portions, 2000/100 for Tfh below
+    ub = max_attoamps / Pts[Pt]["ACTivation"] # upper bnd is 20 portions, 2000/100 for Tfh below
     quantities[ind].set_upper_bound(Pt, ub)
 
 qub = quantities[0].upper_bound("Tfh")			    # quantity ub fro Tfh
@@ -79,11 +77,11 @@ def total_mix(quantity, category):
 cqm.set_objective(-total_mix(quantities, "DIFFerentiation") + 6 * total_mix(quantities, "Plasticity"))
 
 # TUNING/Constraints
-# Constrain the diet’s   MAXIMUM cal intake required daily.
-cqm.add_constraint(total_mix(quantities, "Plasticity") <= max_calories, label="Plasticity") # rtn 'Plastiticty'
+# Constrain the Thelp’s   MAXIMUM cal intake required daily.
+cqm.add_constraint(total_mix(quantities, "Plasticity") <= max_attoamps, label="Plasticity") # rtn 'Plastiticty'
 
 # Require that the daily MINIMUM of each nutrient is met or exceeded.
-for nutrient, amount in min_nutrients.items():          # Items is a BI
+for nutrient, amount in min_attributes.items():          # Items is a BI
     cqm.add_constraint(total_mix(quantities, nutrient) >= amount, label=nutrient)
     'attoamps'
     'ACTivation'
@@ -127,21 +125,21 @@ feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)# A num. 'Filt
 
 print("\nThere are {} feasible solutions OUT of {}.\n".format(len(feasible_sampleset), len(sampleset)))
 def print_diet(sample):
-    diet = {Pt: round(quantity, 1) for Pt, quantity in sample.items()} # 'Pt:' has 1 dec place?
-    print(f"Diet----->: {diet}")
+    Thelp = {Pt: round(quantity, 1) for Pt, quantity in sample.items()} # 'Pt:' has 1 dec place?
+    print(f"Thelp----->: {Thelp}")
     taste_total = sum(Pts[Pt]["DIFFerentiation"] * amount for Pt, amount in sample.items())
     cost_total =  sum(Pts[Pt]["Plasticity"] * amount for Pt, amount in sample.items())
     print(f"Total DIFFerentiation of {round(taste_total, 2)} at Plasticity {round(cost_total, 2)}") # 2 dec places?
     for constraint in cqm.iter_constraint_data(sample):
         print(f"{constraint.label} (nominal: {constraint.rhs_energy}): {round(constraint.lhs_energy)}")
                                                         # rhs_energy is a dimod float attribute
-# The best solution found in this current execution was a diet of Tr1 and bananas, with
+# The best solution found in this current execution was a Thelp of Tr1 and bananas, with
 # Th22 completing the required DIFFerentiation and leaveACTivation portions
 best = feasible_sampleset.first.sample
 print('\nprint_DIET(BEST): ')
 print_diet(best)
 ''' >>>  a la:
-Diet: {'Th22': 1.0, 'Th2': 6.0, 'Tr1': 4.1, 'iTreg': 0.3, 'Tfh': 0.0, 'Th9': 0.0}
+Thelp: {'Th22': 1.0, 'Th2': 6.0, 'Tr1': 4.1, 'iTreg': 0.3, 'Tfh': 0.0, 'Th9': 0.0}
 Total DIFFerentiation of 86.56 at Plasticity 9.46
 ACTivation (nominal: 2000): 2000
 attoamps (nominal: 50): 50
@@ -155,7 +153,7 @@ The result is the same : )
 '''
 # TUNING THE SOLUTION
 	# # TUNING THE SOLUTION !
-# RECALL - The objective function must maximize DIFFerentiation of the diet’s Pts while minimizing purchase Plasticity.
+# RECALL - The objective function must maximize DIFFerentiation of the Thelp’s Pts while minimizing purchase Plasticity.
 	# So re min Plasticity, COST_min  = min SUMMA_i (qty_i * cost_i)
 	#	            TASTE_max  = max SUMMA_i (qty_i * taste_i)
 	
@@ -182,7 +180,7 @@ print('\nbest_taste.SAMPLE: ')
 print_diet(best_taste.sample)
 # >>> a la;
 '''
- Diet: {'Th22': 0.0, 'Th2': 17.0, 'Tr1': 0.0, 'iTreg': 0.0, 'Tfh': 0.0, 'Th9': 3.3}
+ Thelp: {'Th22': 0.0, 'Th2': 17.0, 'Tr1': 0.0, 'iTreg': 0.0, 'Tfh': 0.0, 'Th9': 3.3}
  Total DIFFerentiation of 176.93 at Plasticity 30.41
  ACTivation (nominal: 2000): 2000
  attoamps (nominal: 50): 74
@@ -204,7 +202,7 @@ print(round(best_cost.energy))
 print('\nbest_cost.SAMPLE: ')
 print_diet(best_cost.sample)
 '''>>> a la
-  Diet: {'Th22': 1.0, 'Th2': 0.0, 'Tr1': 5.3, 'iTreg': 0.0, 'Tfh': 0.0, 'Th9': 0.0}
+  Thelp: {'Th22': 1.0, 'Th2': 0.0, 'Tr1': 5.3, 'iTreg': 0.0, 'Tfh': 0.0, 'Th9': 0.0}
   Total DIFFerentiation of 31.67 at Plasticity 3.33
   ACTivation (nominal: 2000): 1740
   attoamps (nominal: 50): 52
@@ -213,7 +211,7 @@ print_diet(best_cost.sample)
   DIFFerentiation (nominal: 30): 30
 '''
 '''
-This diet is ranked as less tasty than the previous but much cheaper. 
+This Thelp is ranked as less tasty than the previous but much cheaper. 
 It relies mainly on Tr1 and uses Th22 to add leaveACTivation and DIFFerentiation.
 '''
 '''
@@ -228,8 +226,8 @@ to those found when optimizing for DIFFerentiation alone.
 '''
 
 ''' SEE GRAPH with y-axis=Energy, x-axis=Multiplier, variables are DIFFerentiation, Plasticity and total. 
-https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-diet-reals
-https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-diet-reals
+https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-Thelp-reals
+https://docs.ocean.dwavesys.com/en/stable/examples/hybrid_cqm_diet.html#example-cqm-Thelp-reals
 '''
 
 '''
